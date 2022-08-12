9976453829c88f204431511d8ca43d98c7ec8de0
Miracle Obafemi <miracleobafemi09@gmail.com>
2022-08-12 17:06:36 +0100

    12/08/2022

--- architecture/screens/Order.js
+++ architecture/screens/Order.js
@@ -2,36 +2,57 @@ import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Alert, TextInput 
 import { useState } from "react";
 import { Button } from "react-native-paper";
 import { Theme } from "../thems/themes";
+import { db } from "../../services/firebase";
+import { setDoc, doc } from "firebase/firestore";
 
-export function Order({ navigation, route }) {
-    // const { orderTotal, orderpizzaName, orderPizzaIngredients, orderSize } = route.params
-    const [firstName,setFirstName] = useState('');
-    const [lastName,setLastName] = useState('');
-    const [address,setAddress] = useState('');
-    const [email,setEmail] = useState('');
-    const [phone,setPhone] = useState('');
+export function Order({ route }) {
+    const { orderTotal, orderpizzaName, orderPizzaIngredients, orderSize } = route.params
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [address, setAddress] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+
+    function create() {
+        const now = new Date();
+        const nowTimeStamp = now.getTime();
+        setDoc(doc(db, 'purchases', 'JRgFlEYcnQ6UY3onMTAa'), {
+            address: address,
+            email: email,
+            firstname: firstName,
+            ingredients: orderPizzaIngredients,
+            lastname: lastName,
+            phone: phone,
+            pizzaname: orderpizzaName,
+            price: orderTotal,
+            size: orderSize,
+            timestamp: nowTimeStamp,
+        })
+    }
     return (
+        
         <View style={styles.container}>
-            {/* First name, Last name, phone, eamil, address */}
-            <TextInput placeholder='First name' style={styles.search} 
-                onPress={()=>{}}
+            <TextInput placeholder='First name' style={styles.search}
+                onPress={(fname) => { setFirstName(fname) }}
             />
-            <TextInput placeholder='Last name' style={styles.search} />
-            <TextInput placeholder='Phone' style={styles.search} />
-            <TextInput placeholder='Email' style={styles.search} />
-            <TextInput placeholder='Address' style={styles.search} />
+            <TextInput placeholder='Last name' style={styles.search}
+                onPress={(lname) => { setLastName(lname) }}
+            />
+            <TextInput placeholder='Phone number' style={styles.search}
+                onPress={(phone) => { setPhone(phone) }}
+            />
+            <TextInput placeholder='Email' style={styles.search}
+                onPress={(email) => { setEmail(email) }}
+            />
+            <TextInput placeholder='Address' style={styles.search}
+                onPress={(address) => { setAddress(address) }}
+            />
 
-            <Button mode="outlined" color="white" style={{
-                marginTop: 20, padding: 20, backgroundColor: Theme.colors.ui.primary
-            }}
-                onPress={() => {
-                    Alert.alert(
-                        'Order Confirmation',
-                        'We have received your customized pizz order.'
-                        [{ text: 'Accept', onPress: () => { console.log('Accept') } }, { text: 'Cancel' }]
-                    )
-                }}
-            >Complete Your Order</Button>
+            <Button mode="outlined" color="white" style={{ marginTop: 20, padding: 20, backgroundColor: Theme.colors.ui.primary }}
+                onPress={create}
+            >
+                Complete Your Order
+            </Button>
         </View>
     )
 }
@@ -44,10 +65,18 @@ const styles = StyleSheet.create({
         marginVertical: Theme.points[2],
         backgroundColor: 'rgba(128, 128, 128, 0.06)',
         fontSize: Theme.points[3],
-        paddingVertical: Theme.points[4] +5,
+        paddingVertical: Theme.points[4] + 5,
         paddingLeft: Theme.points[3],
         borderRadius: 50,
         borderBottomWidth: 1,
         borderBottomColor: Theme.colors.ui.secondary,
     },
 })
+
+/*
+    Alert.alert(
+        'Order Confirmation',
+        'We have received your customized pizz order.'
+        [{ text: 'Accept', onPress: () => { console.log('Accept') } }, { text: 'Cancel' }]
+    )
+*/
